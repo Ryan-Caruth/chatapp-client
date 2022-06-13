e32a40cd63cfe3a61a85ca57b0c67ab214e00cac
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-06-13 16:34:00 -0600

    Added better file structure to react app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import "./App.css";
-//Used to establish a connection between the client and the server.
-import io from "socket.io-client";
-import { useState } from "react";
-
-const socket = io("http://localhost:5000");
+import Room from "./components/rooms/Room";
 
 function App() {
-  const [username, setUserName] = useState("");
-  const [room, setRoom] = useState("");
-
-  //Function to join a room when the user clicks the join button.
-  const joinRoom = () => {
-    if (username !== "" && room !== "") {
-      socket.emit("join-room", room);
-    }
-  };
 
   return (
     <div className="App">
-      <h3>Join A Chat</h3>
-      <input
-        type="text"
-        placeholder="John...."
-        onChange={(e) => {
-          setUserName(e.target.value);
-        }}
-      />
-      <input
-        type="text"
-        placeholder="RoomID..."
-        onChange={(e) => {
-          setRoom(e.target.value);
-        }}
-      />
-      <button onClick={joinRoom}>Join A Room</button>
+      <Room />
     </div>
   );
 }
---
+++ src/components/rooms/Room.js
@@ -0,0 +1,40 @@
+//Used to establish a connection between the client and the server.
+import io from "socket.io-client";
+import { useState } from "react";
+
+const socket = io("http://localhost:5000");
+
+function Room() {
+  const [username, setUserName] = useState("");
+  const [room, setRoom] = useState("");
+
+  //Function to join a room when the user clicks the join button.
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join-room", room);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h3>Join A Chat</h3>
+      <input
+        type="text"
+        placeholder="John...."
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="RoomID..."
+        onChange={(e) => {
+          setRoom(e.target.value);
+        }}
+      />
+      <button onClick={joinRoom}>Join A Room</button>
+    </div>
+  );
+}
+
+export default Room;
