5806edb04f25f5a516795032dfa6f04d864aa07c
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-06-13 16:23:03 -0600

    Client side recognizes a joins room

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import './App.css';
+import "./App.css";
 //Used to establish a connection between the client and the server.
-import io from 'socket.io-client';
+import io from "socket.io-client";
+import { useState } from "react";
 
-const socket = io('http://localhost:5000');
+const socket = io("http://localhost:5000");
 
 function App() {
+  const [username, setUserName] = useState("");
+  const [room, setRoom] = useState("");
+
+  //Function to join a room when the user clicks the join button.
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join-room", room);
+    }
+  };
+
   return (
     <div className="App">
+      <h3>Join A Chat</h3>
+      <input
+        type="text"
+        placeholder="John...."
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="RoomID..."
+        onChange={(e) => {
+          setRoom(e.target.value);
+        }}
+      />
+      <button onClick={joinRoom}>Join A Room</button>
     </div>
   );
 }
